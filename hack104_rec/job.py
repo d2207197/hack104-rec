3be08e62ecd3bb5c7f021fed90205ad17546f68b
Ryan <ryanchao2012@gmail.com>
2018-07-12 20:47:48 +0800

    add job-processed

--- hack104_rec/job.py
+++ hack104_rec/job.py
@@ -1,10 +1,14 @@
 
 from pyspark.sql import functions as f
+from pyspark.sql.types import (ArrayType, LongType, StringType, StructField,
+                               StructType, TimestampType, IntegerType)
 
-from .core import auto_spark
-from .data import Data, DataModelMixin
+from .core import auto_spark, udfy
+from .misc import tokenize
+from .data import Data, DataFormat, DataModelMixin
 from .train_action import TrainActionProcessed
 from .train_click import TrainClickExploded
+from .utils import rm_repeat, to_halfwidth
 
 
 class JobDateRange(DataModelMixin):
@@ -46,3 +50,50 @@ class JobDateRange(DataModelMixin):
                    .otherwise(f.col('action_date_stop')))
                )
         cls.write(sdf)
+
+
+class Job(DataModelMixin):
+    data = Data('job.json', data_format=DataFormat.JSON)
+
+
+class JobProcessed(DataModelMixin):
+    data = Data('job-processed.pq')
+
+    @classmethod
+    @auto_spark
+    def populate(cls, spark=None):
+        sdf = (
+            Job.query(spark)
+            .select('custno',
+                    f.col('jobno').cast(IntegerType()),
+                    'worktime',
+                    'description',
+                    'job',
+                    'others')
+            .withColumn('description', tokenize_to_struct.udf('description'))
+            .withColumn('job', tokenize_to_struct.udf('job'))
+            .withColumn('others', tokenize_to_struct.udf('others'))
+        )
+        cls.write(sdf)
+
+
+@udfy(return_type=StructType([StructField('keyword', StringType()),
+                             StructField('token', ArrayType(StringType()))]))
+def tokenize_to_struct(text):
+    tokens = tokenize(simple_clean(text))
+    return (text, [tok for tok in tokens if len(tok) > 0] if text is not None else None)
+
+
+def simple_clean(text):
+    return (
+        rm_repeat(to_halfwidth(text.replace('\n', ' ').replace('\r', ''))).lower().strip()
+        if text is not None else None
+    )
+
+
+
+if __name__ == '__main__':
+
+    text = 'AAABBBCCCC安安安安哈啊哈哈哈~~~~'
+
+    print(simple_clean(text))
