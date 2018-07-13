95ce2a7229b659525a8139caad151c44d046afc8
顏孜羲 <joseph.yen@gmail.com>
2018-07-13 21:53:07 +0800

    Features cls

--- hack104_rec/model.py
+++ hack104_rec/model.py
@@ -1,67 +1,87 @@
 import attr
+
 from pyspark.sql import functions as f
 
 from .core import auto_spark
-from .job import JobDateRange
+from .data import Data, DataFormat, DataModelMixin
+from .job import JobDateRange, JobProcessed
 from .train_action import TrainActionUnstacked
 from .train_click import TrainClickExploded
 
 
-@auto_spark
-def query_job_features(start_date, stop_date, spark=None):
-    open_days_sdf = (
-        JobDateRange.query(spark=spark)
-        .select(
-            'jobno',
-            f.when(f.col('date_stop') < stop_date, f.col('date_stop'))
-            .otherwise(stop_date).alias('date_stop'),
+class Features(DataModelMixin):
 
-            f.when(f.col('date_start') > start_date, f.col('date_start'))
-            .otherwise(start_date).alias('date_start'),
+    data = Data('features.pq')
+
+    @classmethod
+    @auto_spark
+    def populate(cls, start_date, stop_date, spark=None):
+        open_days_sdf = (
+            JobDateRange.query(spark=spark)
+            .select(
+                'jobno',
+                f.when(f.col('date_stop') < stop_date, f.col('date_stop'))
+                .otherwise(stop_date).alias('date_stop'),
+
+                f.when(f.col('date_start') > start_date, f.col('date_start'))
+                .otherwise(start_date).alias('date_start'),
+            )
+            .withColumn('open_days',
+                        f.datediff(f.col('date_stop'),
+                                   f.col('date_start')) + 1)
         )
-        .withColumn('open_days',
-                    f.datediff(f.col('date_stop'),
-                               f.col('date_start')) + 1)
-    )
 
-    features_sdf = (
-        TrainActionUnstacked.query(spark=spark)
-        .filter(
-            (f.col('date') >= start_date) &
-            (f.col('date') <= stop_date))
-        .groupby('jobno')
-        .sum()
-        .drop('sum(jobno)')
-    )
+        features_sdf = (
+            TrainActionUnstacked.query(spark=spark)
+            .filter(
+                (f.col('date') >= start_date) &
+                (f.col('date') <= stop_date))
+            .groupby('jobno')
+            .sum()
+            .drop('sum(jobno)')
+        )
 
-    count_columns = TrainActionUnstacked.query().columns
-    count_columns.remove('jobno')
-    count_columns.remove('date')
+        count_columns = TrainActionUnstacked.query().columns
+        count_columns.remove('jobno')
+        count_columns.remove('date')
 
-    features_sdf = (
-        features_sdf
-        .join(open_days_sdf, 'jobno')
-    )
-
-    for column in count_columns:
         features_sdf = (
             features_sdf
-            .withColumnRenamed(f'sum({column})', column)
-            .withColumn(f'{column}_per_day',
-                        f.col(column) / f.col('open_days'))
-            .withColumn(f'{column}_per_day',
-                        f.col(column) / f.col('open_days'))
+            .join(open_days_sdf, 'jobno')
         )
 
-    features_sdf = (
-        features_sdf
-        .withColumn('saveJob_per_viewJob',
-                    f.col('viewJob_total') / f.col('saveJob_total'))
-        .withColumn('applyJob_per_viewJob',
-                    f.col('viewJob_total') / f.col('applyJob_total'))
-    )
+        for column in count_columns:
+            features_sdf = (
+                features_sdf
+                .withColumnRenamed(f'sum({column})', column)
+                .withColumn(f'{column}_per_day',
+                            f.col(column) / f.col('open_days'))
+                .withColumn(f'{column}_per_day',
+                            f.col(column) / f.col('open_days'))
+            )
 
-    return features_sdf
+        features_sdf = (
+            features_sdf
+            .withColumn('saveJob_per_viewJob',
+                        f.col('viewJob_total') / f.col('saveJob_total'))
+            .withColumn('applyJob_per_viewJob',
+                        f.col('viewJob_total') / f.col('applyJob_total'))
+        )
+
+        job_sdf = (JobProcessed.query()
+                   .drop('custno', 'description', 'job', 'others'))
+
+        features_sdf = features_sdf.join(job_sdf, 'jobno')
+        features_sdf = (
+            features_sdf
+            .withColumn('start_date', f.lit(start_date))
+            .withColumn('stop_date', f.lit(stop_date)))
+
+        (features_sdf.write
+         .mode('overwrite')
+         .partitionBy('start_date', 'stop_date')
+         .option("compression", "snappy")
+         .parquet('data/features.pq'))
 
 
 @auto_spark(('spark.driver.memory', '10g'),
